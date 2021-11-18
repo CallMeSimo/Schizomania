477b81061bda00fad6a3bc0400de30f8737cb343
Oliverj01 <oliver.jonsson2@elev.ga.ntig.se>
2021-11-18 09:53:11 +0100

    blinkande kod
    skript och kod får objekt att blinka

--- Schizomania/Assets/PickupObjects.cs
+++ Schizomania/Assets/PickupObjects.cs
@@ -4,22 +4,77 @@ using UnityEngine;
 
 public class PickupObjects : MonoBehaviour
 {
+    // GameObject m_Halo;
+    //The Color to be assigned to the Renderer’s Material
+    Color m_NewColor;
+
+    //These are the values that the Color Sliders return
+    float m_Red, m_Blue, m_Green;
+
+    private float timer = 0f;
+
+    private Component halo;
+
+    private bool startloop = false;
+
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
-    }
 
+
+        halo = GetComponent("Halo");
+        halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+
+
+
+    }
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.parent != null)
+
+        if (startloop) 
         {
-            gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-            return;
+        
+            timer += Time.deltaTime;
+            if (timer <= 0.5f)
+            {
+
+                halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+                Debug.Log(timer);
+            }
+            if (timer >= 1f)
+            {
+                halo.GetType().GetProperty("enabled").SetValue(halo, true, null);
+                Debug.Log("2");
+            }
+
+            if (timer >= 2f)
+            {
+                timer -= timer;
+                Debug.Log("3");
+            }
+
+        }else if (!startloop)
+       {
+           halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+            timer -= timer;
+       }
+       if(Input.GetKeyDown(KeyCode.F)){
+startloop=true;
+          }
+
+        else if(Input.GetKeyDown(KeyCode.E)){
+            startloop=false;
+        }
+        
+        
         }
 
-        gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-
     }
-}
+
+
+
+
+
