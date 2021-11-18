264f64029d1c525a4ec93f0e47d3a383f3db32d7
Oliverj01 <oliver.jonsson2@elev.ga.ntig.se>
2021-11-18 15:22:24 +0100

    blinky blink
    blinky blink

--- Schizomania/Assets/PickupObjects.cs
+++ Schizomania/Assets/PickupObjects.cs
@@ -4,22 +4,93 @@ using UnityEngine;
 
 public class PickupObjects : MonoBehaviour
 {
+    // GameObject m_Halo;
+    //The Color to be assigned to the Renderer’s Material
+    Color m_NewColor;
+
+    //These are the values that the Color Sliders return
+    float m_Red, m_Blue, m_Green;
+
+    private float timer = 0f;
+
+    private Component halo;
+
+    private bool insideCol = false;
+    public bool fårBlinka = false;
+
+
     // Start is called before the first frame update
+
     void Start()
     {
-        
-    }
 
+
+        halo = GetComponent("Halo");
+        halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+
+
+
+    }
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.transform.parent != null)
-        {
-            gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-            return;
-        }
-
-        gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            if(fårBlinka)
+            {
+                BLinka();
+            }
 
     }
+            
+    void OnTriggerEnter2D(Collider2D collision) 
+    {   
+        if (collision.tag == "Player"){
+            insideCol = true;
+        }
+
+
+        if (collision.tag == "Player") {
+            insideCol = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+
+            halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+            insideCol = false;
+        }
+    }
+    void BLinka()
+    {
+                if (insideCol)
+        {
+
+            timer += Time.deltaTime;
+            if (timer <= 0.5f)
+            {
+
+                halo.GetType().GetProperty("enabled").SetValue(halo, false, null);
+                Debug.Log(timer);
+            }
+            if (timer >= 1f)
+            {
+                halo.GetType().GetProperty("enabled").SetValue(halo, true, null);
+                Debug.Log("2");
+            }
+
+            if (timer >= 2f)
+            {
+                timer -= timer;
+                Debug.Log("3");
+            }
+
+        }
+    }
+
+
 }
+
+
+
+
+
